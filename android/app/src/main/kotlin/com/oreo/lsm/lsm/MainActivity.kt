1a38e9e6dde26c9308383266b1a60eba1faf1bd8
xamarth <samarth1808kosta@gmail.com>
2025-11-06 02:20:13 +0530

    remdev

--- android/app/src/main/kotlin/com/oreo/lsm/lsm/MainActivity.kt
+++ android/app/src/main/kotlin/com/oreo/lsm/lsm/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.oreo.lsm.lsm
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
--- android/app/src/main/kotlin/com/sysadmin/sysadmin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.prathameshkhade.lsm
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
